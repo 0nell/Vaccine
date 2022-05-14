bfb7922bbbec7f37c26b0760b0e65901047cda0a
seanm75 <sean.mcdonnell1@ucdconnect.ie>
2022-05-14 22:17:27 +0100

    securityConfigBasic+1

--- src/main/java/org/hse/security/SpringSecurityConfiguration.java
+++ src/main/java/org/hse/security/SpringSecurityConfiguration.java
@@ -2,11 +2,13 @@ package org.hse.security;
 
 import org.hse.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -40,5 +42,10 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
