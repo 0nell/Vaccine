e8818085b14f100d596efc2377055371c5c888f3
Lleno <launiv3rse@gmail.com>
2022-05-16 19:34:29 +0100

    Always create session

--- src/main/java/org/hse/security/SpringSecurityConfiguration.java
+++ src/main/java/org/hse/security/SpringSecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -62,6 +63,9 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .headers()
                     .frameOptions()
                     .deny()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 .and()
                 .headers()
                 .xssProtection()
